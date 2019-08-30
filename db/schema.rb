5d256346667b5d7a9a6b00964d8a6f360ea34959
CharlotteDS <charlotte.sacmar@gmail.com>
2019-08-30 14:48:23 +0800

    SNS Authentication and User Avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_032041) do
+ActiveRecord::Schema.define(version: 2019_08_30_054221) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2019_08_30_032041) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "provider"
+    t.string "uid"
+    t.string "image"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
